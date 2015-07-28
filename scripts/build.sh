123abcb85f46a6dc320a0f2f87513c9d92844731
Hajo Nils Krabbenhöft <fxtentacle@googlemail.com>
2015-07-28 12:45:20 +0200

    the only way is binary path patching inside GCJ and ECJ1 .. eeek! but it works :)

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,10 +14,23 @@ echo "Downloading $tarball_url"
 curl -L $tarball_url > t.zip
 unzip t.zip 
 
+echo "Patching GCJ to work"
+cp /app/.apt/usr/bin/gcj-4.8 /app/.apt/usr/bin/gcj-4.8-orig 
+sed -i.bak s/\\/usr\\/share\\/java\\/libgcj-4.8.jar/~usr\\/share\\/java\\/libgcj-4.8.jar/g /app/.apt/usr/bin/gcj-4.8
+ln -s /app/.apt/usr/ ~usr
 
+cp /app/.apt/usr/bin/../lib/gcc/x86_64-linux-gnu/4.8/ecj1 /app/.apt/usr/bin/../lib/gcc/x86_64-linux-gnu/4.8/ecj1-orig
+sed -i.bak s/\\/usr\\/share\\/java/~usr\\/share\\/java/g /app/.apt/usr/bin/../lib/gcc/x86_64-linux-gnu/4.8/ecj1
+
+
+echo "Compiling"
 (
 	cd pdftk-*
 	cd pdftk
+	
+	sed -i.bak s/VERSUFF=-4.6/VERSUFF=-4.8/g Makefile.Debian 
+	sed -i.bak s/\\/usr\\/share\\/java/~usr\\/share\\/java/g Makefile.Debian 
+	
 	make -f Makefile.Debian 
 
 	zip -9r $temp_dir/pdftk.zip $temp_dir/pdftk-*/pdftk

c1da04d9203cea73ea081f77758c3d8e44048254
Hajo Nils Krabbenhöft <fxtentacle@googlemail.com>
2015-07-28 13:02:12 +0200

    fix ~usr symlinks

--- scripts/build.sh
+++ scripts/build.sh
@@ -17,7 +17,6 @@ unzip t.zip
 echo "Patching GCJ to work"
 cp /app/.apt/usr/bin/gcj-4.8 /app/.apt/usr/bin/gcj-4.8-orig 
 sed -i.bak s/\\/usr\\/share\\/java\\/libgcj-4.8.jar/~usr\\/share\\/java\\/libgcj-4.8.jar/g /app/.apt/usr/bin/gcj-4.8
-ln -s /app/.apt/usr/ ~usr
 
 cp /app/.apt/usr/bin/../lib/gcc/x86_64-linux-gnu/4.8/ecj1 /app/.apt/usr/bin/../lib/gcc/x86_64-linux-gnu/4.8/ecj1-orig
 sed -i.bak s/\\/usr\\/share\\/java/~usr\\/share\\/java/g /app/.apt/usr/bin/../lib/gcc/x86_64-linux-gnu/4.8/ecj1
@@ -26,7 +25,12 @@ sed -i.bak s/\\/usr\\/share\\/java/~usr\\/share\\/java/g /app/.apt/usr/bin/../li
 echo "Compiling"
 (
 	cd pdftk-*
+	cd java
+	ln -s /app/.apt/usr/ ~usr
+	cd ..
 	cd pdftk
+	ln -s /app/.apt/usr/ ~usr
+
 	
 	sed -i.bak s/VERSUFF=-4.6/VERSUFF=-4.8/g Makefile.Debian 
 	sed -i.bak s/\\/usr\\/share\\/java/~usr\\/share\\/java/g Makefile.Debian 

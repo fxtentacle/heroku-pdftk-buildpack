1ba7deb672d4d2d7e65ca49abc421063c66e4872
Hajo Nils Krabbenhöft <fxtentacle@googlemail.com>
2021-05-25 18:47:07 +0200

    fix build script for python3

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ temp_dir=$(mktemp -d /tmp/compile.XXXXXXXXXX)
 
 echo "Serving files from /tmp on $PORT"
 cd /tmp
-python -m SimpleHTTPServer $PORT &
+python -m http.server $PORT &
 
 cd $temp_dir
 echo "Temp dir: $temp_dir"
